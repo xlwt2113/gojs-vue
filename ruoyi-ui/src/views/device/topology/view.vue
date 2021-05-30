49a56e386d08d4e76e760c122575b485f7fc15ae
wangtao <xlwt2113@hotmail.com>
2021-05-30 10:34:00 +0800

    增加连线模板

--- ruoyi-ui/src/views/device/topology/view.vue
+++ ruoyi-ui/src/views/device/topology/view.vue
@@ -226,48 +226,88 @@ export default {
     )
 
     // 定义节点连接线样式
-    this.myDiagram.linkTemplate =
-      GO(go.Link, // the whole link panel
-        {
-          routing: go.Link.AvoidsNodes,
-          curve: go.Link.JumpOver,
-          corner: 5, toShortLength: 4,
-          relinkableFrom: true,
-          relinkableTo: true,
-          reshapable: true,
-          resegmentable: true,
-          // mouse-overs subtly highlight links:
-          mouseEnter: function(e, link) {
-            link.findObject('HIGHLIGHT').stroke = 'rgba(30,144,255,0.2)'
-          },
-          mouseLeave: function(e, link) {
-            link.findObject('HIGHLIGHT').stroke = 'transparent'
-          },
-          selectionAdorned: false
+    this.myDiagram.linkTemplate = ('', GO(go.Link, // the whole link panel
+      {
+        routing: go.Link.AvoidsNodes,
+        curve: go.Link.JumpOver,
+        corner: 5, toShortLength: 4,
+        relinkableFrom: true,
+        relinkableTo: true,
+        reshapable: true,
+        resegmentable: true,
+        // mouse-overs subtly highlight links:
+        mouseEnter: function(e, link) {
+          link.findObject('HIGHLIGHT').stroke = 'rgba(30,144,255,0.2)'
         },
-        new go.Binding('points').makeTwoWay(),
-        GO(go.Shape, // the highlight shape, normally transparent
-          { isPanelMain: true, strokeWidth: 8, stroke: 'transparent', name: 'HIGHLIGHT' }),
-        GO(go.Shape, // the link path shape
-          { isPanelMain: true, stroke: 'gray', strokeWidth: 2 },
-          new go.Binding('stroke', 'isSelected', function(sel) {
-            return sel ? 'dodgerblue' : 'gray'
-          }).ofObject()),
-        GO(go.Panel, 'Auto', // the link label, normally not visible
-          { visible: false, name: 'LABEL', segmentIndex: 2, segmentFraction: 0.5 },
-          new go.Binding('visible', 'visible').makeTwoWay(),
-          GO(go.Shape, 'RoundedRectangle', // the label shape
-            { fill: '#F8F8F8', strokeWidth: 0 }),
-          GO(go.TextBlock, 'Yes', // the label
-            {
-              textAlign: 'center',
-              font: '10pt helvetica, arial, sans-serif',
-              stroke: '#333333',
-              editable: true
-            },
-            new go.Binding('text').makeTwoWay())
-        )
+        mouseLeave: function(e, link) {
+          link.findObject('HIGHLIGHT').stroke = 'transparent'
+        },
+        selectionAdorned: false
+      },
+      new go.Binding('points').makeTwoWay(),
+      GO(go.Shape, // the highlight shape, normally transparent
+        { isPanelMain: true, strokeWidth: 8, stroke: 'transparent', name: 'HIGHLIGHT' }),
+      GO(go.Shape, // the link path shape
+        { isPanelMain: true, stroke: 'gray', strokeWidth: 2 },
+        new go.Binding('stroke', 'isSelected', function(sel) {
+          return sel ? 'dodgerblue' : 'gray'
+        }).ofObject()),
+      GO(go.Panel, 'Auto', // the link label, normally not visible
+        { visible: false, name: 'LABEL', segmentIndex: 2, segmentFraction: 0.5 },
+        new go.Binding('visible', 'visible').makeTwoWay(),
+        GO(go.Shape, 'RoundedRectangle', // the label shape
+          { fill: '#F8F8F8', strokeWidth: 0 }),
+        GO(go.TextBlock, 'Yes', // the label
+          {
+            textAlign: 'center',
+            font: '10pt helvetica, arial, sans-serif',
+            stroke: '#333333',
+            editable: true
+          },
+          new go.Binding('text').makeTwoWay())
       )
+    ))
+
+    this.myDiagram.linkTemplateMap.add('link2', GO(go.Link, // the whole link panel
+      {
+        curve: go.Link.Bezier,
+        curviness: 50,
+        relinkableFrom: true,
+        relinkableTo: true,
+        reshapable: true,
+        resegmentable: true,
+        // mouse-overs subtly highlight links:
+        mouseEnter: function(e, link) {
+          link.findObject('HIGHLIGHT').stroke = 'rgba(30,144,255,0.2)'
+        },
+        mouseLeave: function(e, link) {
+          link.findObject('HIGHLIGHT').stroke = 'transparent'
+        },
+        selectionAdorned: false
+      },
+      new go.Binding('points').makeTwoWay(),
+      GO(go.Shape, // the highlight shape, normally transparent
+        { isPanelMain: true, strokeWidth: 8, stroke: 'transparent', name: 'HIGHLIGHT' }),
+      GO(go.Shape, // the link path shape
+        { isPanelMain: true, stroke: 'gray', strokeWidth: 2 },
+        new go.Binding('stroke', 'isSelected', function(sel) {
+          return sel ? 'dodgerblue' : 'gray'
+        }).ofObject()),
+      GO(go.Panel, 'Auto', // the link label, normally not visible
+        { visible: false, name: 'LABEL', segmentIndex: 2, segmentFraction: 0.5 },
+        new go.Binding('visible', 'visible').makeTwoWay(),
+        GO(go.Shape, 'RoundedRectangle', // the label shape
+          { fill: '#F8F8F8', strokeWidth: 0 }),
+        GO(go.TextBlock, 'Yes', // the label
+          {
+            textAlign: 'center',
+            font: '10pt helvetica, arial, sans-serif',
+            stroke: '#333333',
+            editable: true
+          },
+          new go.Binding('text').makeTwoWay())
+      )
+    ))
 
     this.timer = setInterval(this.getDeviceStatus, 25000)
   },
